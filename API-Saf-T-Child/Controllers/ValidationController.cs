de86248f24588cfcc0582cbd2a2acc562176ab34
BLUDUDE02 <stacyscouting@hotmail.com>
2024-03-08 22:27:24 -0500

    Updating User Model, Adding in Email Service Adding in Validdation Email Implementing services in startup and program

--- API-Saf-T-Child/Controllers/ValidationController.cs
+++ API-Saf-T-Child/Controllers/ValidationController.cs
@@ -3,19 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 using API_Saf_T_Child.Models;
 using API_Saf_T_Child.Services;
 using System.Text.RegularExpressions;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API_Saf_T_Child.Controllers
 {
-    [Route("api/device")]
+    [Route("api/validation")]
     public class ValidationController : Controller
     {
         // Private field representing an instance of the MongoDBService class, which will be used to interact with the MongoDB database.
         private readonly MongoDBService _mongoDBService;
+        private readonly MessageService _messageService;
 
         // This constructor injects an instance of MongoDBService into the controller.
-        public ValidationController(MongoDBService mongoDBService)
+        public ValidationController(MongoDBService mongoDBService, MessageService messageService)
         {
             _mongoDBService = mongoDBService;
+            _messageService = messageService;
         }
 
         [HttpGet("checkUsername")]
@@ -47,6 +52,41 @@ namespace API_Saf_T_Child.Controllers
 
             // Return a response based on the availability
             return Ok(isPhoneNumberTaken);
+        }
+
+        [HttpGet("verifyEmailAddress")]
+        public async Task<ActionResult<bool>> VerifyEmailAddress(string id)
+        {
+            var user = await _mongoDBService.GetUserByIdAsync(id);
+            if (user != null)
+            {
+                user.isEmailVerified = true;
+            }
+            return Ok();
+        }
+
+        [HttpPost("sendVerificationEmail")]
+        public async Task<IActionResult> SendVerificationEmail(string id)
+        {
+            var user = await _mongoDBService.GetUserByIdAsync(id);
+            if(user != null && user.Email != null && id != null)
+            {
+                string linkUrl = "http://localhost:4200/api/validation/verifyEmailAddress/" + id;
+                string body = "<p align=\"center\"> Thank you for signing up for Saf - T - Child! </ br > Please click the link below to verify your email! </ br > < a href = \"" + linkUrl + "\" > Verify your Email </ a > </p>";
+                bool success = await _messageService.SendEmail(user.Email, "Verify your Saf-T-Child Email", body);
+                if(success)
+                {
+                    return Ok(success);
+                }
+                else
+                {
+                    return BadRequest("SMTP Failed");
+                }
+            }
+            else
+            {
+                return BadRequest("Invalid User");
+            }
         }
     }
 }
---
+++ API-Saf-T-Child/Services/MessageService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using API_Saf_T_Child.Models;
+using API_Saf_T_Child.Services;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+
+namespace API_Saf_T_Child.Services
+{
+    public class MessageService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public MessageService(IOptions<MailSettings> mailSettingsOptions)
+        {
+            _mailSettings = mailSettingsOptions.Value;
+        }
+
+        public async Task<bool> SendEmail(string to, string subject, string body)
+        {
+            SmtpClient gmailer = new SmtpClient {
+                    Host = _mailSettings.Host,
+                    Port = _mailSettings.Port,
+                    UseDefaultCredentials = false,
+                    EnableSsl = _mailSettings.EnableSSL,
+                    Credentials = new System.Net.NetworkCredential(_mailSettings.Username, _mailSettings.Password)
+                };
+
+            var message = new MailMessage(_mailSettings.Username, to, subject, body);
+            message.IsBodyHtml = true;
+            try
+            {
+                await gmailer.SendMailAsync(message);
+                gmailer.Dispose();
+                return true;
+            }
+            catch
+            {
+                gmailer.Dispose();
+                return false;
+            }
+        }
+    }
+}
