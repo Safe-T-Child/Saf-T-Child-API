b564a2769145e35fb456c5a83bb6f7b3074db752
jonathanh2509 <65786474+jonathanh2509@users.noreply.github.com>
2024-03-24 10:38:45 -0400

    added endpoint , fixed endpoint

--- API-Saf-T-Child/Models/Group.cs
+++ API-Saf-T-Child/Models/Group.cs
@@ -2,8 +2,22 @@
 using MongoDB.Bson.Serialization.Attributes;
 using API_Saf_T_Child.Models;
 
+
+
 namespace API_Saf_T_Child.Models
 {
+    public class UserWithRole
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("role")]
+        public string Role { get; set; }
+    }
     public class Group
     {
         [BsonId]
@@ -17,6 +31,6 @@ namespace API_Saf_T_Child.Models
         public NamedDocumentKey Owner { get; set; }
 
         [BsonElement("users")]
-        public List<NamedDocumentKey> Users { get; set; }
+        public List<UserWithRole> Users { get; set; }
     }
 }
---
+++ API-Saf-T-Child/Models/Login.cs
@@ -0,0 +1,8 @@
+namespace API_Saf_T_Child.Models
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
